71e62eb3aa3fdfb9238b4eb02ee0889c52c56982
abhishek58g <abgun@microsoft.com>
2016-07-18 18:25:07 -0700

    Code Review changes
    Code Review changes

--- src/ADAL.PCL/EventsBase.cs
+++ src/ADAL.PCL/EventsBase.cs
@@ -33,11 +33,16 @@ using System.Threading.Tasks;
 
 namespace Microsoft.IdentityModel.Clients.ActiveDirectory
 {
-    internal class UiEvent : DefaultEvents
+    internal abstract class EventsBase
     {
-        internal UiEvent(string eventName)
-        {
-            //Fill in the default parameters
-        }
+        internal List<Tuple<string, string>> List = new List<Tuple<string, string>>();
+
+        internal static int DefaultEventCount { get; set; }
+
+        internal abstract void SetEvent(string eventName, string eventParameter);
+
+        internal abstract List<Tuple<string, string>> GetEvents(string eventName);
+
+        internal abstract int GetDefaultEventCount();
     }
 }
--- src/ADAL.PCL/Telemetry.cs
+++ src/ADAL.PCL/Telemetry.cs
@@ -1,6 +1,33 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+//----------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
@@ -11,20 +38,25 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
 {
     public class Telemetry
     {
-        private static Telemetry instance = new Telemetry();
+        private readonly static Telemetry Instance = new Telemetry();
 
         public static Telemetry GetInstance()
         {
-                return instance;
+                return Instance;
         }
 
         DefaultDispatcher Dispatcher = new DefaultDispatcher();
 
-        public IDictionary<List<Tuple<string, string>>,string> EventTracking = new ConcurrentDictionary<List<Tuple<string, string>>,string>();
+        private IDictionary<List<Tuple<string, string>>,string> EventTracking = new ConcurrentDictionary<List<Tuple<string, string>>,string>();
 
         string RegisterNewRequest()
         {
             //return the RequestID
+        }
+
+        public void RegisterDispatcher(IDispatcher dispatcher, bool aggregationRequired)
+        {
+            
         }
 
         void StartEvent(string requestId, string eventName)
@@ -32,12 +64,12 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
             
         }
 
-        void StopEvent(string requestId, EventsInterface Event)
+        void StopEvent(string requestId, EventsBase Event)
         {
             
         }
 
-        void DispatchEventNow(string requestID, EventsInterface Event)
+        void DispatchEventNow(string requestID, EventsBase Event)
         {
             
         }
